c9a0046193ff4719575102ef06d4dd6b80c464a8
Manuel <manuel.perales@alu.uclm.es>
2024-11-03 19:00:27 +0100

    It2_5
    --- Issue #8 ---
    Estado previo: sin realizar.
    Nuevo estado: en progreso.
    Notas: aspecto de la interfaz realizado. Pendiente:
    - Recepción y comunicación de datos con Backend

---
+++ riah_frontend/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import Raw_data from "./components/Raw_data_component"
+
+function App() {
+  return (
+    <div className="App">
+       <header className="header">
+        <div className="left-section">
+          <img src={require('./media/RIAH_default_icon.png')} width={60} alt="Logo" className="logo" />
+          <h1 className="title">&nbsp;RIAH</h1>
+        </div>
+        <div className="right-section">
+          <button className="nav-button">Home</button>
+          <button className="nav-button">Sobre RIAH</button>
+          <button className="nav-button">Mi portal</button>
+          <img src={require('./media/RIAH_profile.png')} alt="Profile" className="profile-pic" />
+        </div>
+      </header>
+      <Raw_data></Raw_data>
+      <footer className="footer">
+        <div className="footer-nav">
+          <button className="footer-button">Sobre nosotros</button>
+          <button className="footer-button">Política de privacidad</button>
+          <button className="footer-button">Contáctanos</button>
+        </div>
+      </footer>
+      <div className="rights-sub-banner">
+        &copy; 2024 Grupo AIR - Todos los derechos reservados
+      </div>
+    </div>
+  );
+}
+
+export default App;
